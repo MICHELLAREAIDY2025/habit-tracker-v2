f5885bfb177c7355a6cb3e0e5b41b9a59f8abcbe
Jinane <jinanealayli@gmail.com>
2025-01-26 22:06:34 +0200

    add icon folder

---
+++ habit-tracker-v2/src/data/icons.js
@@ -0,0 +1,17 @@
+import { FaRunning, FaBook, FaDumbbell, FaPaintBrush, FaHeart, FaMusic, FaCode, FaGamepad } from 'react-icons/fa';
+import { GiCookingPot, GiPrayerBeads } from 'react-icons/gi';
+
+const icons = [
+  { name: 'Running', icon: <FaRunning /> },
+  { name: 'Book', icon: <FaBook /> },
+  { name: 'Dumbbell', icon: <FaDumbbell /> },
+  { name: 'Paint Brush', icon: <FaPaintBrush /> },
+  { name: 'Heart', icon: <FaHeart /> },
+  { name: 'Music', icon: <FaMusic /> },
+  { name: 'Code', icon: <FaCode /> },
+  { name: 'Cooking', icon: <GiCookingPot /> },
+  { name: 'Gaming', icon: <FaGamepad /> },
+  { name: 'Spiritual Practices', icon: <GiPrayerBeads /> },
+];
+
+export default icons;
